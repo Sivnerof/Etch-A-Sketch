36e214ea124c4407034609df7209a2b7fbc473d1
Rogue Blue <rogueblueprint@protonmail.com>
2022-02-21 20:24:07 -0700

    testing

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 /* Grab Elements */
 
-
-for (let i = 0; i < 16; i++){
-    const containerDiv = document.querySelector("#container");
-    const verticalDiv = document.createElement('div');
-    verticalDiv.classList.add('vertical-div');
-    containerDiv.appendChild(verticalDiv);
-    for (let j = 0; j < 16; j++){
-        const containerDiv = document.querySelector("#container");
-        const horizontalDiv = document.createElement('div');
-        const para = document.createElement('p');
-        para.innerText = `${j} of ${i}`;
-        horizontalDiv.appendChild(para);
-        horizontalDiv.classList.add('horizontal-div');
-        containerDiv.appendChild(horizontalDiv);
+function originalGrid(num=16){
+    for (let i = 0; i < num; i++){
+        const sixteenGrid = document.querySelector('#sketch-grid');
+        const div = document.createElement('div');
+        div.textContent = `${i + 1}`;
+        sixteenGrid.appendChild(div);
     }
 }
+
+originalGrid();
