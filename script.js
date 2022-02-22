0711037aa35724a83acf3fec0ccd567230924f0f
Rogue Blue <rogueblueprint@protonmail.com>
2022-02-22 00:16:38 -0700

    Fixed spacing issues

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ const grid_Div = document.querySelector('#sketch-grid');
 /* Functions */
 function createGrid(num=16){
     num = num > 100 || num < 16 ? 16 : num;
-
     for (let i = 0; i < num; i++){
         const row_Div = document.createElement('div');
         row_Div.classList.add('row-div');
-        row_Div.textContent = `Row Number: ${i + 1}`;
         grid_Div.appendChild(row_Div);
 
         for (let j = 0; j < num; j++){
             const col_Div = document.createElement('div');
             col_Div.classList.add('col-div');
-            col_Div.textContent = `Column Number: ${j + 1}`;
             row_Div.appendChild(col_Div);
         }
     }
